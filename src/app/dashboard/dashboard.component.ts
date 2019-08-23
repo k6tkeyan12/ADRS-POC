55ce5e592f5f942f5879c700edf604c96295eb93
k6tkeyan12 <k6tkeyan12@gmail.com>
2019-08-23 15:14:21 +0530

    update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 import * as Chartist from 'chartist';
 
 @Component({
@@ -8,143 +12,175 @@ import * as Chartist from 'chartist';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
-  startAnimationForLineChart(chart){
-      let seq: any, delays: any, durations: any;
-      seq = 0;
-      delays = 80;
-      durations = 500;
+  accidentData = [];
+  totalaccident = 0;
+  curentMonthAccident = 0;
+  averageReportingTime = 0;
+  totalLifeSaves = 0;
+  mapLatLong = [];
+  constructor(private http: Http) { }
+  startAnimationForLineChart(chart) {
+    let seq: any, delays: any, durations: any;
+    seq = 0;
+    delays = 80;
+    durations = 500;
 
-      chart.on('draw', function(data) {
-        if(data.type === 'line' || data.type === 'area') {
-          data.element.animate({
-            d: {
-              begin: 600,
-              dur: 700,
-              from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
-              to: data.path.clone().stringify(),
-              easing: Chartist.Svg.Easing.easeOutQuint
-            }
-          });
-        } else if(data.type === 'point') {
-              seq++;
-              data.element.animate({
-                opacity: {
-                  begin: seq * delays,
-                  dur: durations,
-                  from: 0,
-                  to: 1,
-                  easing: 'ease'
-                }
-              });
+    chart.on('draw', function (data) {
+      if (data.type === 'line' || data.type === 'area') {
+        data.element.animate({
+          d: {
+            begin: 600,
+            dur: 700,
+            from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+            to: data.path.clone().stringify(),
+            easing: Chartist.Svg.Easing.easeOutQuint
           }
-      });
+        });
+      } else if (data.type === 'point') {
+        seq++;
+        data.element.animate({
+          opacity: {
+            begin: seq * delays,
+            dur: durations,
+            from: 0,
+            to: 1,
+            easing: 'ease'
+          }
+        });
+      }
+    });
 
-      seq = 0;
+    seq = 0;
   };
-  startAnimationForBarChart(chart){
-      let seq2: any, delays2: any, durations2: any;
+  startAnimationForBarChart(chart) {
+    let seq2: any, delays2: any, durations2: any;
 
-      seq2 = 0;
-      delays2 = 80;
-      durations2 = 500;
-      chart.on('draw', function(data) {
-        if(data.type === 'bar'){
-            seq2++;
-            data.element.animate({
-              opacity: {
-                begin: seq2 * delays2,
-                dur: durations2,
-                from: 0,
-                to: 1,
-                easing: 'ease'
-              }
-            });
-        }
-      });
+    seq2 = 0;
+    delays2 = 80;
+    durations2 = 500;
+    chart.on('draw', function (data) {
+      if (data.type === 'bar') {
+        seq2++;
+        data.element.animate({
+          opacity: {
+            begin: seq2 * delays2,
+            dur: durations2,
+            from: 0,
+            to: 1,
+            easing: 'ease'
+          }
+        });
+      }
+    });
 
-      seq2 = 0;
+    seq2 = 0;
   };
   ngOnInit() {
-      /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
+    /* Read file accident file */
 
-      const dataDailySalesChart: any = {
-          labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-          series: [
-              [12, 17, 7, 17, 23, 18, 38]
-          ]
-      };
-
-     const optionsDailySalesChart: any = {
-          lineSmooth: Chartist.Interpolation.cardinal({
-              tension: 0
-          }),
-          low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
+    this.getAccidentData().subscribe(data => {
+      this.accidentData = data
+      this.totalaccident = this.accidentData.length;
+      this.curentMonthAccident = 0;
+      this.averageReportingTime = 10;
+      this.totalLifeSaves = 10;
+      for ( var accident of this.accidentData){
+        this.mapLatLong.push({
+          'name': accident.owner,
+          'lat': accident.lat,
+          'long': accident.long
+        });
       }
+      console.log(this.accidentData,'----------',this.mapLatLong);
+    }, error => console.log(error));
 
-      var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+    /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
+    const dataDailySalesChart: any = {
+      labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+      series: [
+        [12, 17, 7, 17, 23, 18, 38]
+      ]
+    };
 
-      this.startAnimationForLineChart(dailySalesChart);
+    const optionsDailySalesChart: any = {
+      lineSmooth: Chartist.Interpolation.cardinal({
+        tension: 0
+      }),
+      low: 0,
+      high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+      chartPadding: { top: 0, right: 0, bottom: 0, left: 0 },
+    }
 
+    var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
 
-      /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
-
-      const dataCompletedTasksChart: any = {
-          labels: ['12p', '3p', '6p', '9p', '12p', '3a', '6a', '9a'],
-          series: [
-              [230, 750, 450, 300, 280, 240, 200, 190]
-          ]
-      };
-
-     const optionsCompletedTasksChart: any = {
-          lineSmooth: Chartist.Interpolation.cardinal({
-              tension: 0
-          }),
-          low: 0,
-          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
-      }
-
-      var completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
-
-      // start animation for the Completed Tasks Chart - Line Chart
-      this.startAnimationForLineChart(completedTasksChart);
+    this.startAnimationForLineChart(dailySalesChart);
 
 
+    /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
 
-      /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
+    const dataCompletedTasksChart: any = {
+      labels: ['Cars', 'Two-Wheler', 'Heavy Vehicles'],
+      series: [
+        [15, 443, 2]
 
-      var datawebsiteViewsChart = {
-        labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
-        series: [
-          [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
+      ]
+    };
 
-        ]
-      };
-      var optionswebsiteViewsChart = {
-          axisX: {
-              showGrid: false
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: { top: 0, right: 5, bottom: 0, left: 0}
-      };
-      var responsiveOptions: any[] = [
-        ['screen and (max-width: 640px)', {
-          seriesBarDistance: 5,
-          axisX: {
-            labelInterpolationFnc: function (value) {
-              return value[0];
-            }
+    const optionsCompletedTasksChart: any = {
+      lineSmooth: Chartist.Interpolation.cardinal({
+        tension: 0
+      }),
+      low: 0,
+      high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+      chartPadding: { top: 0, right: 0, bottom: 0, left: 0 }
+    }
+
+    var completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
+
+    // start animation for the Completed Tasks Chart - Line Chart
+    this.startAnimationForLineChart(completedTasksChart);
+
+
+
+    /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
+
+    var datawebsiteViewsChart = {
+      labels: ['Minor', 'Moderate', 'Severe'],
+      series: [
+        [542, 443, 320]
+
+      ]
+    };
+    var optionswebsiteViewsChart = {
+      axisX: {
+        showGrid: false
+      },
+      low: 0,
+      high: 1000,
+      chartPadding: { top: 0, right: 5, bottom: 0, left: 0 }
+    };
+    var responsiveOptions: any[] = [
+      ['screen and (max-width: 640px)', {
+        seriesBarDistance: 5,
+        axisX: {
+          labelInterpolationFnc: function (value) {
+            return value[0];
           }
-        }]
-      ];
-      var websiteViewsChart = new Chartist.Bar('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
+        }
+      }]
+    ];
+    var websiteViewsChart = new Chartist.Bar('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
 
-      //start animation for the Emails Subscription Chart
-      this.startAnimationForBarChart(websiteViewsChart);
+    //start animation for the Emails Subscription Chart
+    this.startAnimationForBarChart(websiteViewsChart);
+  }
+
+
+  getAccidentData(): Observable<any> {
+    return this.http.get("assets/accident/1.json")
+      .map((res: any) => res.json());
+    // .catch((error:any) => console.log(error));
+
   }
 
 }
--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,125 +1,118 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
 
 declare const google: any;
 
 interface Marker {
-lat: number;
-lng: number;
-label?: string;
-draggable?: boolean;
+    lat: number;
+    lng: number;
+    label?: string;
+    draggable?: boolean;
 }
 @Component({
-  selector: 'app-maps',
-  templateUrl: './maps.component.html',
-  styleUrls: ['./maps.component.css']
+    selector: 'app-maps',
+    templateUrl: './maps.component.html',
+    styleUrls: ['./maps.component.css']
 })
-export class MapsComponent implements OnInit {
+export class MapsComponent implements OnChanges, OnInit {
 
-  constructor() { }
+    @Input() accidentData: any;
+    @Input() mapLatLong: any;
+    _accidentData = [];
+    _mapLatLong = [];
+    constructor() { }
+    ngOnChanges(changes: SimpleChanges) {
+        this._accidentData = changes.accidentData.currentValue;
+        this._mapLatLong = changes.mapLatLong.currentValue;
+    }
 
-  ngOnInit() {
+    ngOnInit() {
+        setTimeout(() => {
+            // var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
+            var mapOptions = {
+                zoom: 8,
+                center: { lat: -33.9, lng: 151.2 }
+            };
+            var image = {
+                url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+                size: new google.maps.Size(20, 32),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(0, 32)
+            };
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
-    var mapOptions = {
-        zoom: 13,
-        center: myLatlng,
-        scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-        styles: [{
-            "featureType": "water",
-            "stylers": [{
-                "saturation": 43
-            }, {
-                "lightness": -11
-            }, {
-                "hue": "#0088ff"
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "hue": "#ff0000"
-            }, {
-                "saturation": -100
-            }, {
-                "lightness": 99
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#808080"
-            }, {
-                "lightness": 54
-            }]
-        }, {
-            "featureType": "landscape.man_made",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#ece2d9"
-            }]
-        }, {
-            "featureType": "poi.park",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#ccdca1"
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "labels.text.fill",
-            "stylers": [{
-                "color": "#767676"
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "labels.text.stroke",
-            "stylers": [{
-                "color": "#ffffff"
-            }]
-        }, {
-            "featureType": "poi",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }, {
-            "featureType": "landscape.natural",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "visibility": "on"
-            }, {
-                "color": "#b8cb93"
-            }]
-        }, {
-            "featureType": "poi.park",
-            "stylers": [{
-                "visibility": "on"
-            }]
-        }, {
-            "featureType": "poi.sports_complex",
-            "stylers": [{
-                "visibility": "on"
-            }]
-        }, {
-            "featureType": "poi.medical",
-            "stylers": [{
-                "visibility": "on"
-            }]
-        }, {
-            "featureType": "poi.business",
-            "stylers": [{
-                "visibility": "simplified"
-            }]
-        }]
+            for (var index of this._mapLatLong) {
+                console.log('index', index);
+                var marker = new google.maps.Marker({
+                    position: { lat: parseInt(index.lat), lng: parseInt(index.long) },
+                    map: map,
+                    icon: image,
+                    // shape: shape,
+                    title: index.name
+                });
+            }
 
-    };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+            console.log('--------marker', marker);
+            // To add the marker to the map, call setMap();
+            marker.setMap(map);
 
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        title: "Hello World!"
-    });
 
-    // To add the marker to the map, call setMap();
-    marker.setMap(map);
-  }
+
+            // var map = new google.maps.Map(document.getElementById('map'), {
+            //     zoom: 10,
+            //     center: {lat: -33.9, lng: 151.2}
+            // });
+            
+            // setMarkers(map);
+              
+            //   // Data for the markers consisting of a name, a LatLng and a zIndex for the
+            //   // order in which these markers should display on top of each other.
+              
+              
+            //   function setMarkers(map) {
+            //     var beaches = [
+            //         ['Bondi Beach', -33.890542, 151.274856, 4],
+            //         ['Coogee Beach', -33.923036, 151.259052, 5],
+            //         ['Cronulla Beach', -34.028249, 151.157507, 3],
+            //         ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+            //         ['Maroubra Beach', -33.950198, 151.259302, 1]
+            //       ];
+            //     // Adds markers to the map.
+              
+            //     // Marker sizes are expressed as a Size of X,Y where the origin of the image
+            //     // (0,0) is located in the top left of the image.
+              
+            //     // Origins, anchor positions and coordinates of the marker increase in the X
+            //     // direction to the right and in the Y direction down.
+            //     var image = {
+            //       url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+            //       // This marker is 20 pixels wide by 32 pixels high.
+            //       size: new google.maps.Size(20, 32),
+            //       // The origin for this image is (0, 0).
+            //       origin: new google.maps.Point(0, 0),
+            //       // The anchor for this image is the base of the flagpole at (0, 32).
+            //       anchor: new google.maps.Point(0, 32)
+            //     };
+            //     // Shapes define the clickable region of the icon. The type defines an HTML
+            //     // <area> element 'poly' which traces out a polygon as a series of X,Y points.
+            //     // The final coordinate closes the poly by connecting to the first coordinate.
+            //     var shape = {
+            //       coords: [1, 1, 1, 20, 18, 20, 18, 1],
+            //       type: 'poly'
+            //     };
+            //     for (var i = 0; i < beaches.length; i++) {
+            //       var beach = beaches[i];
+            //       var marker = new google.maps.Marker({
+            //         position: {lat: beach[1], lng: beach[2]},
+            //         map: map,
+            //         icon: image,
+            //         shape: shape,
+            //         title: beach[0],
+            //         zIndex: beach[3]
+            //       });
+            //     }
+            //   }
+
+        }, 2000);
+    }
 
 }
