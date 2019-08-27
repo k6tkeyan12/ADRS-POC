60b25a61f84b9f4d31f0c60233c6f61288ba7a4f
Mariselvam Thalavan -X (mthalava - HCL TECHNOLOGIES LIMITED at Cisco) <mthalava@partnet.cisco.com>
2019-08-27 16:31:49 +0530

    Table list change

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,15 +1,142 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import {MatTableDataSource} from '@angular/material/table';
+//import {MatDialogModule} from '@angular/material/dialog';
+import {MatDialog, MatDialogRef,MAT_DIALOG_DATA} from '@angular/material/dialog';
 
+export interface PeriodicElement {
+  vehicle: string;
+  owner: string;
+  contact: number;
+  severity: string;
+}
+export interface DialogData {
+  time: string;
+  date: string;
+  lat:string;
+  long:string;
+  vehicle: string;
+  owner: string;
+  contact: number;
+  severity: string;
+  owneradd:string;
+  noperson:number;
+  image:any;
+  audio:string;
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
 @Component({
   selector: 'app-table-list',
   templateUrl: './table-list.component.html',
   styleUrls: ['./table-list.component.css']
 })
 export class TableListComponent implements OnInit {
+ 
+  time: string;
+  date: string;
+  lat:string;
+  long:string;
+  accidentdate:string;
+  policeaddress:string;
+  vehicle: string;
+  owner: string;
+  contact: number;
+  severity: string;
+  owneradd:string;
+  noperson:number;
+  image:any;
+  audio:string;
+  
 
-  constructor() { }
+  displayedColumns: string[] = ['vehicle', 'owner','contact','severity'];
+
+  accidentData = [];
+  accidentDatareport = [];
+  dataSource:any;
+  //dataSource = new MatTableDataSource(this.accidentData);
+
+  
+
+  constructor(private http: Http,public dialog: MatDialog) { }
 
   ngOnInit() {
+    this.getAccidentData().subscribe(data => {
+      console.log("datatablellist");
+      this.accidentData = data;
+      this.dataSource = new MatTableDataSource(data);
+      console.log(this.dataSource);
+      /*this.accidentData = data
+      this.totalaccident = this.accidentData.length;
+      this.curentMonthAccident = 0;
+      this.averageReportingTime = 10;
+      this.totalLifeSaves = 10;
+      for ( var accident of this.accidentData){
+        this.mapLatLong.push({
+          'name': accident.owner,
+          'lat': accident.lat,
+          'long': accident.long
+        });
+      }
+      console.log(this.accidentData,'----------',this.mapLatLong);*/
+    }, error => console.log(error));
+  }
+  getAccidentData(): Observable<any> {
+    return this.http.get("assets/accident/1.json")
+      .map((res: any) => res.json());
+    // .catch((error:any) => console.log(error));
+
+  }
+  accidentreportpopup(acc){
+    //alert("popreport");
+    console.log(acc);
+    this.accidentDatareport=[];
+    this.accidentdate=acc.date;
+    this.time=acc.time;
+    this.lat=acc.lat;
+    this.long=acc.long;
+    this.policeaddress=acc.policeaddress;
+    this.vehicle= acc.vehicle;
+    this.owner= acc.owner;
+    this.contact= acc.contact;
+    this.severity= acc.severity;
+    this.owneradd=acc.owneradd;
+    this.noperson=acc.noperson;
+    this.image=acc.image;
+    this.audio=acc.audio;
+
+    debugger;
+    this.openDialog();
+  }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '600px',
+      data: {date: this.accidentdate, time: this.time,lat:this.lat,long:this.long,policeaddress:this.policeaddress,vehicle:this.vehicle,owner:this.owner,contact:this.contact,severity:this.severity,owneradd:this.owneradd,noperson:this.noperson,image:this.image,audio:this.audio}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.time = result;
+    });
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    console.log(this.dataSource);
+  }
 }
